0d69c45e2756c7440b8111e2b8381e305420aa41
Jason Huey <jason.huey1@gmail.com>
2018-09-02 16:39:21 -0700

    update

--- js/Main.js
+++ js/Main.js
@@ -8,10 +8,17 @@ var todo = new Vue({
 		addTask: function() {
 			var task = this.newTask;
 			this.todoList.push(task);
+			// This will reset the input text on the input line	
+			this.newTask= '';
 		},
 		removeTask: function(task) {
 			var index = this.todoList.indexOf(task);
 			this.todoList.splice(index, 1);
-		}
+		},
+		clearAll: function() {
+			//Reset the List, using an array versus an empty ''
+			this.todoList = [];
+		},
+		
 	}
 });
